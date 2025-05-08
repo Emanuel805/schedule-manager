9f867d184f3b9d74b0a53a3ec2af26504eab8832
Emanuel805 <trandafiremanuel440@gmail.com>
2025-05-08 22:55:48 +0300

    Creating task repo

--- src/main/java/com/emanuel805/schedulemanager/model/Task.java
+++ src/main/java/com/emanuel805/schedulemanager/model/Task.java
@@ -18,4 +18,43 @@ public class Task {
     private LocalDate deadline;
     private int priority;
 
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
